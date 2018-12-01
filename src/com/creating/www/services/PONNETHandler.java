2aa782ea9cd84dfc747083d7003e8f7d82da3656
hack0303 <1097037585@qq.com>
2018-12-01 11:27:00 +0800

    implements Handler

---
+++ src/com/creating/www/services/PONNETHandler.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.creating.www.services;
+
+import java.util.List;
+
+import com.creating.www.AlarmModel;
+
+/**
+ * @author Chack Yao
+ * @firstcreatetime 2018年12月1日
+ *
+ */
+public class PONNETHandler implements Handler {
+
+	/* (non-Javadoc)
+	 * @see com.creating.www.services.Handler#findSource()
+	 */
+	@Override
+	public AlarmModel findSource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.creating.www.services.Handler#findDescends()
+	 */
+	@Override
+	public List<AlarmModel> findDescends() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
